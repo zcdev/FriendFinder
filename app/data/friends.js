f392be2491a38abd61b3929741f0933ec29bb485
ZCDEV <zcdev@calvisitor-10-105-173-180.calvisitor.1918.berkeley.edu>
2018-12-19 09:24:00 -0800

    Upload files

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friends = [
+    {
+    "name":"Rubber Duck",
+    "photo":"duckfriend.jpg",
+    "scores":[
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+  }
+]
+
+module.exports = friends;
+  
