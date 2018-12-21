4236d687a4bd76cc4d09893457ad978002d9705f
ZCDEV <zcdev@Orange.attlocal.net>
2018-12-20 18:11:25 -0800

    Add algorithm

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,33 @@
 var friends = [
     {
     "name":"Rubber Duck",
-    "photo":"duckfriend.jpg",
+    "photo":"https://raw.githubusercontent.com/zcdev/FriendFinder/master/app/data/duckfriend.jpg",
     "scores":[
+        "3",
+        "1",
+        "3",
         "5",
         "1",
         "4",
+        "1",
+        "5",
+        "2",
+        "5"
+      ]
+  },
+  {
+    "name":"ZCDEV",
+    "photo":"https://avatars2.githubusercontent.com/u/15851427?s=460&v=4",
+    "scores":[
+        "3",
+        "5",
+        "3",
+        "1",
+        "5",
         "4",
         "5",
         "1",
         "2",
-        "5",
-        "4",
         "1"
       ]
   }
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,31 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
+      var matchedFriend = {
+        name: "",
+        photo: "",
+        differences: Infinity
+      };
+
+      var userScores = req.body.scores;
+
+      for (var i = 0; i < friends.length; i++){
+        var currentFriend = friends[i];
+        var differences = 0;
+        var currentFriendScores = friends[i].scores;
+
+        for (var j = 0; j < currentFriendScores.length; j++) {
+          differences += Math.abs(currentFriendScores[j] - userScores[j]);
+        }
+        //console.log("Difference between: " + currentFriend.name + "and " + req.body.name + " = " + differences);
+
+        if (differences <= matchedFriend.differences) {
+          matchedFriend.name = currentFriend.name;
+          matchedFriend.photo = currentFriend.photo;
+          matchedFriend.differences = differences;
+        }
+      }
       friends.push(req.body);
-      res.json(true);
+      res.json(matchedFriend);
   });
 };
