710fc816f6d0ea36d035d75c456f06129e7f21ab
ZCDEV <zcdev@Orange.attlocal.net>
2019-04-14 10:53:14 -0700

    Add background image css

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
